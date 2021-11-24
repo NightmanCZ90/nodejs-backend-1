04c526de6d14ad7c556db50d80bbe30ed43feb14
NightmanCZ90 <novak.jan90@gmail.com>
2021-11-24 23:53:56 +0100

    refactor: Create controllers folder and move controllers

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,10 @@
 const path = require('path')
-
 const express = require('express')
-
-const rootDir = require('../util/path')
+const productsController = require('../controllers/products')
 
 const router = express.Router()
 
-const products = []
+router.get('/add-product', productsController.getAddProduct)
+router.post('/add-product', productsController.postAddProduct)
 
-router.get('/add-product', (req, res, next) => {
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    activeAddProduct: true,
-    productCSS: true,
-    formCSS: true,
-    layout: 'main-layout',
-  })
-})
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title })
-  res.redirect('/')
-})
-
-exports.routes = router
-exports.products = products
+module.exports = router
