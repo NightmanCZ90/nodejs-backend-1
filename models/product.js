16d0056276a9755a73e44071bb49daa73c9a32fd
NightmanCZ90 <novak.jan90@gmail.com>
2021-11-27 18:12:21 +0100

    refactor: Refactor getting products from file

--- models/product.js
+++ models/product.js
@@ -1,6 +1,17 @@
 const fs = require('fs')
 const path = require('path')
 
+const p = path.join(__dirname, '..', 'data', 'products.json')
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([])
+    }
+    cb(JSON.parse(fileContent))
+  })
+}
+
 module.exports = class Product {
   constructor(title) {
     this.title = title
@@ -8,28 +19,16 @@ module.exports = class Product {
 
   // class method on instantiated object
   save() {
-    const p = path.join(__dirname, '..', 'data', 'products.json')
-    fs.readFile(p, (err, fileContent) => {
-      let products = []
-      if (!err) {
-        products = JSON.parse(fileContent)
-      }
+    getProductsFromFile((products) => {
       products.push(this)
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err)
       })
     })
-
   }
 
   // static keyword allows us to call the method on this class without instantiating it - Product.fechtAll
   static async fetchAll(cb) {
-    const p = path.join(__dirname, '..', 'data', 'products.json')
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([])
-      }
-      cb(JSON.parse(fileContent))
-    })
+    getProductsFromFile(cb)
   }
 }
