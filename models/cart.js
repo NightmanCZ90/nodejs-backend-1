38d5d14968512e2c618d7a5f29210ba7f69fd9e8
NightmanCZ90 <novak.jan90@gmail.com>
2021-12-04 22:30:51 +0100

    fix: Fix creating multiple carts for one user on refresh

--- models/cart.js
+++ models/cart.js
@@ -1,6 +1,11 @@
 const Sequelize = require('sequelize').Sequelize
 const sequelize = require('../util/database')
 
-const Cart = sequelize.define('cart')
+const Cart = sequelize.define('cart', {
+  userId: {
+    type: Sequelize.INTEGER,
+    unique: true
+  }
+})
 
 module.exports = Cart
