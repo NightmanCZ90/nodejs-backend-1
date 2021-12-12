8507df4fc4e513988b21b9063b35b40e13872fb5
NightmanCZ90 <novak.jan90@gmail.com>
2021-12-12 22:56:58 +0100

    feature: Add CSRF protection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf')
 const path = require('path')
 const errorController = require('./controllers/error')
 const User = require('./models/user')
@@ -14,6 +15,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions',
 })
+const csrfProtection = csrf()
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
@@ -25,6 +27,7 @@ const authRoutes = require('./routes/auth')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store, }))
+app.use(csrfProtection)
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -36,6 +39,12 @@ app.use((req, res, next) => {
       next()
     })
     .catch(err => console.log(err))
+})
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  res.locals.csrfToken = req.csrfToken()
+  next()
 })
 
 app.use('/admin', adminRoutes)
