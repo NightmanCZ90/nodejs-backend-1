e2aca2c88411fe82e7f14fd42f3adec88123528f
NightmanCZ90 <novak.jan90@gmail.com>
2021-12-12 22:59:01 +0100

    fix: Fix changing user name to email error

--- controllers/shop.js
+++ controllers/shop.js
@@ -85,7 +85,7 @@ exports.postOrder = (req, res, next) => {
       const products = user.cart.items.map(i => ({ quantity: i.quantity, product: { ...i.productId._doc } }))
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user
         },
         products: products,
