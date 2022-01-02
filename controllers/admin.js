4289b3a30cb42ecfa93a2cd6b0d5154de41ec8dc
NightmanCZ90 <novak.jan90@gmail.com>
2022-01-02 19:42:15 +0100

    feature: Deleting product with DOM manipulation

--- controllers/admin.js
+++ controllers/admin.js
@@ -153,8 +153,8 @@ exports.postEditProduct = (req, res, next) => {
     })
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const { productId } = req.body;
+exports.deleteProduct = (req, res, next) => {
+  const { productId } = req.params;
   Product.findById(productId).then(product => {
     if (!product) {
       return next(new Error('Product not found.'));
@@ -163,12 +163,10 @@ exports.postDeleteProduct = (req, res, next) => {
     return Product.deleteOne({ _id: productId, userId: req.user._id })
   })
     .then(() => {
-      res.redirect('/admin/products');
+      res.status(200).json({ message: 'Success' });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: 'Deleting product failed.' });
     })
 }
 
